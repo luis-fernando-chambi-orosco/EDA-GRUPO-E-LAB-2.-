4d9aab068ce0c02c772283c0a903767d7c769cf6
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-05-28 12:49:15 -0500

    commit de los comentarios del problema 3

--- problema_3.java
+++ problema_3.java
@@ -5,19 +5,22 @@ public class problema_3 {
 		/** */
 		// Procedimiento para imprimir triángulo
 		/** */
+		// pones un condcionasl para que la bariable base no sea  un numero negativo
 		if (base > 0) {
-			trianguloRecursivo(base - 1);
+			// aplicamos recursividad es decir llamaramos el metodo dentro del mismo metodo 
+			trianguloRecursivo(base - 1);// gracias a esto en un monento llegara a ser cero la variable 
 			for (int i = 0; i < base; i++) {
-				System.out.print("*");
+				System.out.print("*");// imprimimos el asterisco
 			}
-			System.out.println();
+			System.out.println();// esto hara que cuando acabe el for se pasa a la siguiente linea 
 		}
 
 	}
-
 	public static void main(String args[]) {
+		// // creamos un objeto de la clase problema_3
 		problema_3 arr = new problema_3();
 		int b = 5;
+		// usamos el metodo trianguloRecursivo y le damos como parametro la variable b
 		arr.trianguloRecursivo(b);
 
 	}
