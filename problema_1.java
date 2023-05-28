d8520d8dc1a1a775fe132f504734961332a9ec14
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-05-28 12:11:37 -0500

    commit aumento de comentarios problema 1

--- problema_1.java
+++ problema_1.java
@@ -2,8 +2,11 @@ package lab2;
 import java.util.Arrays;
 public class problema_1 {
 	public static void main(String args[]) {
+		// creamos un objetio de la clase problema_1
 		problema_1 arr = new problema_1();
+		// cremaos un array
 		int[] numeros = { 1, 2, 3,};
+		// imprimimos y llamamos al metodos invertirArrray con el parametro numeros 
 		  System.out.println("Array invertido: " + Arrays.toString(arr.invertirArray(numeros)));
 	}
 
@@ -11,7 +14,9 @@ public class problema_1 {
 		/** */
 		// Procedimiento para invertir la matriz
 		/** */
+		// creamos un nuevo array 
 		int[] Ain = new int[A.length];
+		// dentro de este nuevo array guardaremos el array invertido 
 		for (int i = 0; i < A.length; i++) {
 			Ain[A.length - 1 - i] = A[i];
 		}
