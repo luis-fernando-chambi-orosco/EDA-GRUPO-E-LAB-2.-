5ec28a77f4d13647eaf964257160096cc9cd8667
Chambi Orosco Luis Fernando <lchambio@unsa.edu.pe>
2023-05-28 14:10:03 -0500

    commit problema 4.3

---
+++ list.java
@@ -0,0 +1,22 @@
+public class list<T> {
+    private Node<T> root; //referencia sobre el nodo inicial 
+    private int size;// tamaña  de la lista 
+
+    public list() {
+        root = null;
+        size = 0;
+    }
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        // Getters y setters
+        // ...
+    }
+}
